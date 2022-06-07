d35eb15810a06dafbd94def620f282f7c47b0fed
ayancn <96911869+ayancn@users.noreply.github.com>
2022-06-07 19:52:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+sed -i 's/luci-lib-ipkg/luci-base/g' package/luci-app-ssr-plus/Makefile
