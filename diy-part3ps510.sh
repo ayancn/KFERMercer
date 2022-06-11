38f0c00aff1cc0ef719d01c0ff88ae5b6d974bed
ayancn <96911869+ayancn@users.noreply.github.com>
2022-06-12 07:55:38 +0800

    Update diy-part3ps510.sh

--- diy-part3ps510.sh
+++ diy-part3ps510.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+sed -i 's/5.4/5.10/g' target/linux/ramips/Makefile
